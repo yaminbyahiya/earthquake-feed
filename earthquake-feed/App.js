3cba4a25724defc0262886eeb57a099b253f45c3
yaminbyahiya <yaminbyahiya@gmail.com>
2023-09-27 03:40:45 +0600

    Integrated USGS API

--- earthquake-feed/App.js
+++ earthquake-feed/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView, StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -17,13 +17,26 @@ function HomeScreen({navigation}) {
 }
 
 function DetailsScreen() {
+
+  let start_time = '2023-09-01';
+  let end_time = '2023-09-27';
+
+  const [data, setData] = useState(null);
+
+  useEffect(()=>{
+    const apiUrl = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${start_time}&endtime=${end_time}&minmagnitude=5`;
+    fetch(apiUrl).then(response => response.json()).then(jsonData => setData(jsonData)).catch(error => console.error('Error: ', error)); 
+  }, []);
+
   return (
-    // <View style={styles.header}>
-    //   <Text>Details Screen</Text>
-    // </View>
     <ScrollView>
-      <View style={styles.header}><Text>Recent Earthquakes</Text></View>
-      
+      {data && data.features && data.features.map((feature, index) => (
+        <View key={index} style={styles.earthquakeCard}>
+          <Text style={styles.magnitude}>Magnitude: {feature.properties.mag}</Text>
+          <Text style={styles.place}>Place: {feature.properties.place}</Text>
+          <Text style={styles.time}>Time: {new Date(feature.properties.time).toString()}</Text>
+        </View>
+      ))}
     </ScrollView>
   );
 }
@@ -32,10 +45,6 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <NavigationContainer>{
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
@@ -59,5 +68,22 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems:'center',
     justifyContent:'flex-start'
-  }
+  },
+  earthquakeCard: {
+    backgroundColor: 'white',
+    padding: 10,
+    margin: 10,
+    borderRadius: 8,
+    elevation: 2,
+  },
+  magnitude: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  place: {
+    fontSize: 14,
+  },
+  time: {
+    fontSize: 14,
+  },
 });
