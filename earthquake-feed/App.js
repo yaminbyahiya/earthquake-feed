095e3238655d8b9e4cac46142c4dbe709e3418f1
yaminbyahiya <yaminbyahiya@gmail.com>
2023-09-26 23:40:08 +0600

    Integrated Native Stack Navigator & Navigation Container

--- earthquake-feed/App.js
+++ earthquake-feed/App.js
@@ -1,12 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { ScrollView, StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={styles.header}>
+      <Text>Earthquake Live Feed</Text>
+      <Button
+        title="View Recent Earthquakes"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    // <View style={styles.header}>
+    //   <Text>Details Screen</Text>
+    // </View>
+    <ScrollView>
+      <View style={styles.header}><Text>Recent Earthquakes</Text></View>
+      
+    </ScrollView>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <NavigationContainer>{
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+        <Stack.Screen name="Details" component={DetailsScreen} options={{title: 'Recent Earthquakes'}}/>
+      </Stack.Navigator>
+    }
+    </NavigationContainer>
+
+
   );
 }
 
@@ -17,4 +55,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    flex: 1,
+    alignItems:'center',
+    justifyContent:'flex-start'
+  }
 });
